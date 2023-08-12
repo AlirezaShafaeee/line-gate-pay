875207c702c9c1b3667e06d95117c417385ed836
ailrezashafaee <alirezashafaee@aut.ac.ir>
2023-08-13 00:43:11 +0330

    responsive added

--- src/pages/Home/Components/Header/Navbar.tsx
+++ src/pages/Home/Components/Header/Navbar.tsx
@@ -12,9 +12,22 @@ const active: number = 0
 const Navbar = () => {
   return (
     <nav className="text-text flex justify-between items-center">
-      <div className="flex items-center space-x-12 max-xl:space-x-7">
+      <div className="flex items-center space-x-12 max-xl:space-x-7 max-md:justify-between max-md:w-full">
+        <button className="w-[42px] h-[42px] bg-card_light rounded-xl justify-center items-center  max-md:flex hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="16"
+            viewBox="0 0 20 16"
+            fill="none"
+          >
+            <rect y="12" width="20" height="4" rx="2" fill="#1093FF" />
+            <rect y="6" width="20" height="4" rx="2" fill="#99D1FF" />
+            <rect width="20" height="4" rx="2" fill="#1093FF" />
+          </svg>
+        </button>
         <Brand className="max-xl:w-[100px]" />
-        <ul className="flex space-x-12 max-xl:space-x-8 max-lg:space-x-6">
+        <ul className="flex space-x-12 max-xl:space-x-8 max-lg:space-x-6 max-lg:hidden">
           {routes.map((value: string, index: number) => {
             return (
               <li
@@ -33,7 +46,7 @@ const Navbar = () => {
           })}
         </ul>
       </div>
-      <div className="flex space-x-2">
+      <div className="flex space-x-2 max-md:hidden">
         <button className="bg-background shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-xl max-xl:rounded-lg max-xl:shadow-sm max-xl:px-6 max-xl:py-2 px-9 py-3 max-lg:px-7 max-lg:text-[12px] font-bold text-sm">
           Login
         </button>
--- src/pages/Home/Sections/CoinRow.tsx
+++ src/pages/Home/Sections/CoinRow.tsx
@@ -44,6 +44,20 @@ const CoinRow = () => {
     autoplaySpeed: 1000,
     pauseOnHover: true,
     focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          pauseOnHover: true,
+          focusOnSelect: true,
+        },
+      },
+    ],
   }
   return (
     <Slider {...settings}>
